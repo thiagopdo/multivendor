a476a135010da412651be191155b6509cfe60ffc
thiagopdo <thiago.falconer@gmail.com>
2025-05-23 12:54:06 -0300

    feat: add pages for About, Contact, Features, and Pricing; implement layout and navbar components

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -62,7 +62,7 @@ const navbarItems = [
 export function Navbar() {
 	const pathname = usePathname();
 
-	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
 	return (
 		<nav className="h-20 flex border-b justify-between font-medium bg-white">
