fdd9b628f93359783edfa0c407f69e37147c4625
thiagopdo <thiago.falconer@gmail.com>
2025-06-17 18:47:01 -0300

    fix(auth): enable cookie settings for cross-site usage in generateAuthCookie function

--- src/modules/auth/utils.ts
+++ src/modules/auth/utils.ts
@@ -13,8 +13,8 @@ export async function generateAuthCookie({ prefix, value }: Props) {
     value: value,
     httpOnly: true,
     path: "/",
-    // sameSite: "none",
-    // domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
-    // secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
+    secure: process.env.NODE_ENV === "production",
   });
 }
