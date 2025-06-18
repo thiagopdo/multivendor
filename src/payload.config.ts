58a7b31e5d8d16ed5a57838d4578fa8065f37699
thiagopdo <thiago.falconer@gmail.com>
2025-06-18 11:12:54 -0300

    feat(cart): update CartButton link to use environment variable for URL feat(media): add tenant field to Media interface and MediaSelect type feat(payload: add media collection to multiTenantPlugin configuration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -58,6 +58,7 @@ export default buildConfig({
     multiTenantPlugin<Config>({
       collections: {
         products: {},
+        media: {},
       },
       tenantsArrayField: {
         includeDefaultField: false,
